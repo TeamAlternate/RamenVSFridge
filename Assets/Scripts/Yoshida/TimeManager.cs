df735989aa35b8ed80b21844b1ed2a8b7b71ebfa
jec-25ci0333 <25ci0333@jec.ac.jp>
2026-07-01 16:25:43 +0900

    時間管理修正、イベント追加

--- Assets/Scripts/Yoshida/TimeManager.cs
+++ Assets/Scripts/Yoshida/TimeManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public enum TimeState
 {
@@ -14,14 +15,17 @@ public class TimeManager : MonoBehaviour
 {
     [SerializeField] private float timeMax = 180.0f;
 
-    private float time = 180.0f;
+    public float time { get; private set; }
     private float accelerate = 1.5f;
     private float decelerate = 0.75f;
     private TimeState state = TimeState.Normal;
+    private bool isTimeup = false;
 
     private KeyCode DebugAccele = KeyCode.F1;
     private KeyCode DebugDecele = KeyCode.F2;
 
+    // 時間切れイベント
+    public event Action OnTimeup;
 
     /// <summary>
     /// 起動時処理
@@ -40,6 +44,7 @@ public class TimeManager : MonoBehaviour
     public void Initialize()
     {
         time = timeMax;
+        isTimeup = false;
     }
 
     /// <summary>
@@ -47,17 +52,51 @@ public class TimeManager : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        ChangeTimeState(TimeState.Normal);
-        if( Input.GetKey(DebugAccele))
+        isTimeup = IsTimeupCheck();
+
+        // デバッグ用(本来であれば時間ステートはラーメン側から行われる)
         {
-            ChangeTimeState(TimeState.Accele);
-        }
-        if( Input.GetKey(DebugDecele))
-        {
-            ChangeTimeState(TimeState.Decele);
+            ChangeTimeState(TimeState.Normal);
+            if (Input.GetKey(DebugAccele))
+            {
+                ChangeTimeState(TimeState.Accele);
+            }
+            if (Input.GetKey(DebugDecele))
+            {
+                ChangeTimeState(TimeState.Decele);
+            }
         }
 
-        TimeDecrease();
+        if (!isTimeup)
+        {
+            TimeDecrease();
+
+            Debug.Log(time);
+        }
+    }
+
+    /// <summary>
+    /// 時間切れチェック(イベント発火元)
+    /// </summary>
+    /// <returns>
+    /// 時間切れかどうか
+    /// </returns>
+    private bool IsTimeupCheck()
+    {
+        bool onTimeup = time < 0.0f;
+        if( onTimeup )
+        {
+            var timeEvent = OnTimeup;
+            if (timeEvent != null)
+            {
+                // イベント発火
+                OnTimeup();
+            }
+
+            Debug.Log("TIME UP");
+        }
+
+        return onTimeup;
     }
 
     /// <summary>
