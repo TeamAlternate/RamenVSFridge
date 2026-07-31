2991af05c29678d07e5165ba1b3e8bf3e2d159e4
sqrqmi <286570095+jec-25ci0333@users.noreply.github.com>
2026-07-31 14:26:08 +0900

    TitleStarter実装

--- Assets/Scripts/Yoshida/TitleStarter.cs
+++ Assets/Scripts/Yoshida/TitleStarter.cs
@@ -1,3 +1,4 @@
+using Scenes;
 using UnityEngine;
 
 /// <summary>
@@ -5,6 +6,23 @@ using UnityEngine;
 /// </summary>
 public class TitleStarter : MonoBehaviour
 {
+    private GameObject titleManagerGameObject;
+    private float startStayTime = 5.0f;
+    private float startRemainingTime;
+    private bool isStayRamen = false;
+    private bool isStayFridge = false;
+
+    private void Awake()
+    {
+        titleManagerGameObject = GameObject.Find("Manager");
+        if( !titleManagerGameObject )
+        {
+            Debug.LogWarning("TitleManager not Find");
+        }
+
+        startRemainingTime = startStayTime;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +32,56 @@ public class TitleStarter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TimeRemaining();
     }
 
-    private void OnTriggerStay(Collider other)
+    private void TimeRemaining()
     {
-        
+        if( isStayRamen && isStayFridge )
+        {
+            startRemainingTime -= Time.deltaTime;
+            StartGame();
+        }
+        else
+        {
+            startRemainingTime = startStayTime;
+        }
+
+        Debug.Log(startRemainingTime);
+    }
+
+    private void StartGame()
+    {
+        titleManagerGameObject.GetComponent<TitleManager>().StartGame();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bool isRamen = other.gameObject.tag == "Ramen";
+        bool isFridge = other.gameObject.tag == "Fridge";
+
+        if( isRamen )
+        {
+            isStayRamen = true;
+        }
+        if( isFridge )
+        {
+            isStayFridge = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        bool isRamen = other.gameObject.tag == "Ramen";
+        bool isFridge = other.gameObject.tag == "Fridge";
+
+        if (isRamen)
+        {
+            isStayRamen = false;
+        }
+        if (isFridge)
+        {
+            isStayFridge = false;
+        }
     }
 }
